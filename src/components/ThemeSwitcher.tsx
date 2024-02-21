c11774c062c51360888d2de7c318fb9fcdc49901
alevante <alejandrovelasquezmolina@gmail.com>
2024-02-20 20:34:14 -0500

    feat: auth with clerk working and adding dark and light theme selector

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -3,7 +3,7 @@
 import { useTheme  } from "next-themes";
 import React, { useEffect, useState } from "react";
 import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
-import { SunIcon } from "@radix-ui/react-icons";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 
 function ThemeSwitcher() {
     const {theme, setTheme} = useTheme();
@@ -19,6 +19,9 @@ function ThemeSwitcher() {
         <TabsList>
             <TabsTrigger value="light" onClick={()=> setTheme("light")}>
                 <SunIcon className="h-[1.2rem] w-[1.2rem]" />
+            </TabsTrigger>
+            <TabsTrigger value="dark" onClick={()=> setTheme("dark")}>
+                <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
             </TabsTrigger>
         </TabsList>
     </Tabs>

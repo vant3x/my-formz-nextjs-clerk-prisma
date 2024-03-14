a0002ff727b6fffe7f600051c9e7123e4493cfac
alevante <alejandrovelasquezmolina@gmail.com>
2024-03-14 13:03:24 -0500

    feat: adding draggable function base and styles drop

--- src/components/FormBuilderAndPreview/DesignerContextArea.tsx
+++ src/components/FormBuilderAndPreview/DesignerContextArea.tsx
@@ -3,9 +3,13 @@
 import { useDroppable } from "@dnd-kit/core";
 
 import DesignerSidebarControl from "./DesignerSidebarControl";
+import { cn } from "@/lib/utils";
+import { FormElementInstance } from "./FormBuilder/FormElements";
+import { useState } from "react";
 
 export default function Designer() {
 
+    const [elements, setElements] = useState<FormElementInstance[]>([]);
     const droppable = useDroppable({
         id: "design-drop-area",
         data: {
@@ -16,9 +20,20 @@ export default function Designer() {
     return (
         <div className="flex w-full h-full">
             <div className="p-4 w-full">
-                <div className="bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto">
+                <div
+                ref={droppable.setNodeRef}
+                className={cn("bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto", droppable.isOver && "ring-2 ring-primary/20")}>
                     
-                        <p className="text-muted-foreground text-3xl flex flex-grow items-center font-bold">Drop here</p>
+                      {
+                        !droppable.isOver && (
+                            <p className="text-muted-foreground text-3xl flex flex-grow items-center font-bold">Drop here</p>
+                        )
+                      }
+                        {droppable.isOver && (
+                            <div className="p-4 w-full">
+                                <div className="h-[120px] rounded-md bg-primary/20"></div>
+                            </div>
+                        )}
                     </div>    
             </div>
             <DesignerSidebarControl/>

c51bb28843cde6e3b8123ce52a24421eaf58ba2d
alevante <alejandrovelasquezmolina@gmail.com>
2024-03-21 08:57:33 -0500

    feat: adding idGenerator function

---
+++ src/components/FormBuilderAndPreview/Designer/DesignerElementWrapper.tsx
@@ -0,0 +1,7 @@
+import { FormElementInstance } from "../FormBuilder/FormElements";
+
+export default function DesignerElementWrapper({element}: { element: FormElementInstance}) {
+    return (
+        <>Designer Element wrapper</>
+    )
+}
